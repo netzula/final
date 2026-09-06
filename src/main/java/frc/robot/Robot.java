92d9d5a85d5c43a4c753c55517be1f835d56a593
netzula <renatacarbajalramirez@gmail.com>
2026-09-05 23:28:48 -0600

    final

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,24 +4,32 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Rotations;
-import static edu.wpi.first.units.Units.Volts;
 
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 
-import edu.wpi.first.math.controller.PIDController;
-import com.ctre.phoenix6.signals.PIDOutput_PIDOutputModeValue;
-import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog.MotorLog;
+
+//NO queremos el set enonces ponemos, porque trabaja con -1 a 1 y queremos 2.5
+
+
+
+//este es el motor
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+
+
+
+
+// Control de xbox
+import  edu.wpi.first.wpilibj.XboxController;
+
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -29,28 +37,106 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
-  private final TalonFX chasis = new TalonFX(1);
+
+  
+  private Command m_autonomousCommand;
+
+  //la clase debe ir privada porque es algo que no se puede mostrar.
+
+  //estos son los cuatros motores del chasis
+  //motores del lado izq
+  private final TalonFX rueda1iz = new TalonFX(1);
+  private final TalonFX rueda2de = new TalonFX(2);
+
+
+  //motores del lado derecho
+  private final TalonFX rueda3iz = new TalonFX(3);
+  private final TalonFX rueda4de = new TalonFX(4);
+
+
+  //declaramos el modo autonnomo que es el tiempo
   private final Timer autoTimer = new Timer();
-  private final TalonFX turret = new TalonFX(2);
-  private final TalonFX flywheel = new TalonFX(3);
-  private final VoltageOut reqVoltage = new VoltageOut(0);
-  private final VelocityVoltage reqVelocidad = new VelocityVoltage(0);
-  private final PositionVoltage reqPosicion = new PositionVoltage(0);
+  
+  //Aqui declaramos que el voltage 
+  private final PositionVoltage reqPositionVoltage = new PositionVoltage(0);
+  private final TalonFX motor = new TalonFX(9);
+  private final VelocityVoltage flywheelsVoltage = new VelocityVoltage(0);
+  private final TalonFX angulador = new TalonFX(10);
+
+
+  private final TalonFX indexwheel = new TalonFX(13);
+
+
+  //estos son los flywheel
+  private final TalonFX flywheel1 = new TalonFX(11);
+  private final TalonFX flywheel2 = new TalonFX(12);
+
 
   //relleno
   private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private static final String kcustomAuto = "My auto";
+  private final SendableChooser<String> m_Chooser = new SendableChooser<>();
+
+
+
+
+  private final RobotContainer m_robotContainer;
+
+  //Control del xbox
+  XboxController control = new XboxController(0);
+
+  //velocidad flywheels y el del boton b
+  double rpm = 2500.0;
+  double rps = rpm/ 60.0;
+
+  //los rps no los ocupamos pero por si acaso, para una ayuda visual
+  //el rpm para la fase 5
+  double rpm2 = 1000.0;
+  double rps2 = rpm2 / 60.0;
+
+  //rpm del boton Y
+  double rpm3 = 1800.0;
+  double rps3 = rpm3 / 60.0;
+
+  //modo desatasco
+  double rpm4 = -800.0;
+  double rps4 = rpm4 / 60.0;
+  
+
+
+ 
+  
+  //ROTACION
+  double rotacionGrados = 25.0;
+  double anguloVuelta = rotacionGrados / 360.0;
+
+  //ANGULACIÓN
+  double anguloGrados = 30.0;
+  
+  //ROTACIONTELEOP y angulos
+  double rotacionGrados2= 15.0;
+   double rotacionVuelta2= rotacionGrados2/ 360.0;
+     double anguloGrados2= 20.0;
+     double rotacionGrados3= 15.0;
+   double rotacionVuelta3= rotacionGrados3/ 360.0;
+        double anguloGrados3= 30.0;
+
+  
+  
+
 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   public Robot() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+ 
+   
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+    m_Chooser.setDefaultOption(kcustomAuto, kDefaultAuto);
+    SmartDashboard.putData("Auto choices", m_Chooser);
   }
 
   /**
@@ -61,91 +147,242 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    //Inicio del temporizador que va comenzar desde cero
     autoTimer.reset();
     autoTimer.start();
-    double velocidadRPM = 2500;
-    double aungulosGrados = 90;
-    double rotaciones = aungulosGrados / 360.0;
-  
-    //PIDController pid = new PIDController(kDefaultPeriod, kDefaultPeriod, kDefaultPeriod);
+
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    }
   }
 
   /** This function is called periodically during autonomous. */
-  //hacer la conversion de rotacion manual
   @Override
-  public void autonomousPeriodic() {{
-    //cambiar numeros
-    //1
-        if (autoTimer.get()<0){
-      chasis.setVoltage(2.5);}
-      //2
-      else if (autoTimer.get()<2.5){
-      chasis.setVoltage(0.0);
-      turret.setControl(reqPosicion.withPosition(90));}
+  public void autonomousPeriodic() {
 
-      //3
-      else if (autoTimer.get()<3.5){
-        chasis.setVoltage(6);}
-       //5
-      else if (autoTimer.get()<7.5){
-      turret.setControl(reqPosicion.withPosition(30));
-      turret.setControl(reqPosicion.w);}
-      //fase6
-     else if (autoTimer.get()<15.0);
-    chasis.stopMotor();
+    //fase 1
+    if (autoTimer.get() >=0.0 && autoTimer.get() < 2.5){
+
+       rueda1iz.setVoltage(2.5);
+       rueda2de.setVoltage(2.5);
+       rueda3iz.setVoltage(2.5);
+       rueda4de.setVoltage(2.5);
+      
     }
+
+      //fase 2
+
+      else if (autoTimer.get() >= 2.5 && autoTimer.get() < 3.5){
+        rueda1iz.setVoltage(6);
+          rueda2de.setVoltage (-6);
+          //lado derecha
+           rueda3iz.setVoltage(6);
+           rueda4de.setVoltage(-6);
+           //flywheels
+           motor.setVoltage(0);
+           angulador.setVoltage(0);
+
+      }
+
+
+    // fase 3
+     else if (autoTimer.get() >= 3.5 && autoTimer.get() < 7.5) {
+      //como no podiamos poner las coordenadas exactas hicimos una aproximación con voltaje, hasta donde podia llega.    
+      rueda1iz.setVoltage(6); 
+       rueda2de.setVoltage(-6); 
+        rueda3iz.setVoltage(6);  
+        rueda4de.setVoltage(-6);
+    
+
+      //2500 rpm
+      flywheel1.setControl(flywheelsVoltage.withVelocity(rpm));
+      flywheel2.setControl(flywheelsVoltage.withVelocity(rpm));
+     }
+
+      //fase 4
+      else if (autoTimer.get() >= 7.5 && autoTimer.get() < 10.5) {
+       rueda1iz.setVoltage(3);
+       rueda2de.setVoltage(-3);
+
+       //CHECA LOS VOLTIOS
+
+       rueda3iz.setVoltage(3);
+       rueda4de.setVoltage(-3);
+       
+       motor.setVoltage(0);
+       angulador.setVoltage(0);
+      }
+      // FASE 5: 10.5 - 15.0 segundos
+else if (autoTimer.get() >= 10.5 && autoTimer.get() < 15.0) {
+
+  // CHASIS DETENIDO
+  rueda1iz.setVoltage(0);
+  rueda2de.setVoltage(0);
+  rueda3iz.setVoltage(0);
+  rueda4de.setVoltage(0);
+
+  // ROTACIÓN DE LA TORRETA A 25°
+  motor.setControl(
+      reqPositionVoltage.withPosition(anguloVuelta)
+  );
+
+  // ANGULACIÓN A 30°
+  angulador.setControl(
+      reqPositionVoltage.withPosition(anguloVuelta)
+  );
+
+  // INDEX A 1000 RPM
+  indexwheel.setControl(
+      indexwheel.withVelocity(rps2)
+  );
+}
+
+
+      //FASE 6
+
+      
+      else if (autoTimer.get() >= 15.0 && autoTimer.get() < 20.0) {
+        //CHASIS APAGADO
+        rueda1iz.setVoltage(0);
+        rueda2de.setVoltage(0);
+        rueda3iz.setVoltage(0);
+        rueda4de.setVoltage(0);
+
+        //TORRETA YA HASTA AHÍ
+        flywheel1.setVoltage(0);
+        flywheel2.setVoltage(0);
+      
+        //INDEX YA BYE
+        indexwheel.setVoltage(0);
+
+        //ANGULO 
+       motor.setVoltage(0);
+        angulador.setVoltage(0);
+      }
+    
     
 
 
 
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
 
-  /** This function is called once when teleop is enabled. */
+    }
+
+
+
+
+
   @Override
   public void teleopInit() {
-        autoTimer.reset();
-    autoTimer.start();
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    
+    }
   }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
-      
+  public void teleopPeriodic(){
+    //Xbox pasa al teleoperado 
+  if (control.getYButton()){
+
+    //15 de motor y 20 de angulacion 
+    flywheel1.set(rpm3);
+    flywheel2.set(rpm3);
+    motor.setControl(reqPositionVoltage.withPosition(anguloVuelta));
+     angulador.setControl(reqPositionVoltage.withPosition(anguloGrados)); 
+  } 
+
+     if (control.getBButton()) {
+    //ocupa 25 de motor y 30 de angulacion
+    flywheel1.set(rpm);
+    flywheel2.set(rpm);
     
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+    
+    //DESATASCO 
+    if (control.getRightTriggerAxis() < 1){   
+       flywheel1.set(rpm4);      
+       flywheel2.set(rpm4);}
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
 
-  /** This function is called once when test mode is enabled. */
+    if (control.getLeftY()>0.1){
+       rueda1iz.set(control.getLeftY());
+       rueda2de.set(control.getLeftY());   
+       rueda4de.set(control.getLeftY());  
+       rueda3iz.set(control.getLeftY());}
+
+      else if (control.getLeftY()<-0.1){
+        rueda1iz.set(control.getLeftY());
+        rueda2de.set(control.getLeftY());
+        rueda4de.set(control.getLeftY());
+        rueda3iz.set(control.getLeftY());
+
+      }
+      else {
+         rueda1iz.set(0);
+         rueda2de.set(0);
+         rueda4de.set(0);
+         rueda3iz.set(0);
+     }
+     }
+      
+     
+     if (control.getLeftX() > 0.1) {
+       rueda1iz.set(control.getLeftX());
+        rueda3iz.set(control.getLeftX());
+        rueda2de.set(-control.getLeftX());
+        rueda4de.set(-control.getLeftX());}
+
+       else if (control.getLeftX() < -0.1) {
+    rueda1iz.set(-control.getLeftX());
+    rueda3iz.set(-control.getLeftX());
+    rueda2de.set(control.getLeftX());
+    rueda4de.set(control.getLeftX());}
+    else {
+    rueda1iz.set(0);
+    rueda3iz.set(0);
+    rueda2de.set(0);
+    rueda4de.set(0);
+
+
+  
+  }
+
+
+
+   }
+
+  
+
   @Override
-  public void testInit() {}
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
 
   /** This function is called periodically during test mode. */
   @Override
@@ -158,4 +395,8 @@ public class Robot extends TimedRobot {
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
+
+
+
+
 }
